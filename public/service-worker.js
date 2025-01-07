001a19791f1ec5c447c80594dbb78bc7c696bbd0
erissonmcc <erissonmiqueas@gmail.com>
2025-01-07 06:00:43 -0300

    App adicionado

---
+++ public/service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'v1_cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'styles/index.css',
+  'scripts/index.js',
+  'assets/images/icon.png'
+];
+
+// Instala o Service Worker e faz o cache dos arquivos essenciais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Arquivos em cache durante a instalação');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativa o Service Worker e limpa caches antigos, se necessário
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta requisições e serve conteúdo a partir do cache, se possível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        // Retorna a resposta do cache, ou faz a requisição à rede
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
